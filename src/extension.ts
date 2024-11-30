258fcde2288354837a8948fd1fbf73e1c8552302
huangshanhuo <2898815932@qq.com>
2024-11-30 15:02:21 +0800

    update:暂时不启用悬浮提示功能

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import { initUserConfig } from './config/userConfig'
 import initCommand from './command'
-import initHoverProvider from './hoverProvider'
+// import initHoverProvider from './hoverProvider'
 import initSnippets from './snippets'
 import { findFilesFromWorkspace } from './utils'
 import * as vscode from 'vscode'
@@ -14,7 +14,7 @@ export function activate(this: any, context: vscode.ExtensionContext) {
     if (!packageJsonPathList.length) return // 如果没有package.json文件，直接返回
     initUserConfig(context) // 初始化用户配置
     initCommand.call(this, vscode, context) // 初始化所有命令
-    initHoverProvider(context) // 初始化所有悬浮提示
+    // initHoverProvider(context) // 初始化所有悬浮提示
     initSnippets(context) // 初始化代码片段
   })()
 }
