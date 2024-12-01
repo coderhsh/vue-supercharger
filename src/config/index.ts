0d6a3f1905fe9143eb46c7179ea4d7fc0d75e4c6
coderhsh <85278170+coderhsh@users.noreply.github.com>
2024-12-01 18:12:02 +0800

    chore:优化类型

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,14 +2,14 @@ import { env } from 'vscode'
 /** 用户的语言 */
 const userLanguage = env.language
 /** 扩展内部的配置 */
-export const extensionConfig: Record<string, string> & { extensionLanguage: 'en' | 'zh' } = {
+export const extensionConfig = {
   /** 扩展名称 */
   extensionName: 'Vue Supercharger',
   /** 扩展id */
   extensionId: 'vue-supercharger',
   /** 扩展显示的语言(目前只支持中英文 zh|en) */
   extensionLanguage: userLanguage.startsWith('zh') ? 'zh' : 'en',
-}
+} as const
 /** 提供给用户的命令列表 */
 export const userCommandList = {
   /** 选择要支持的vue版本 */
