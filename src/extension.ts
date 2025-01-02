33707b89322e63ddcbb39a58bbd3a3616d29b0da
huangshanhuo <2898815932@qq.com>
2025-01-02 17:48:16 +0800

    delete:删除无用代码

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,7 @@ import initCommand from './command'
 // import initHoverProvider from './hoverProvider'
 import initSnippets from './snippets'
 import * as vscode from 'vscode'
-import localize from './localize'
 export function activate(this: any, context: vscode.ExtensionContext) {
-  console.log(localize('command.selectVueVersion.title')) // 为了执行国际化代码
   // 获取当前工作区的根目录（通常是项目根目录）
   const workspaceFolders = vscode.workspace.workspaceFolders
   // 如果没有工作区根目录，直接返回
