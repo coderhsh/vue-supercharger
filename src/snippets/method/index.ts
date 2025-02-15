fe06b775dc47ea518dabb097e9a37181911aa5bd
huangshanhuo <2898815932@qq.com>
2025-02-15 11:46:41 +0800

    fix(method): 处理Vue版本关键字匹配问题

--- src/snippets/method/index.ts
+++ src/snippets/method/index.ts
@@ -138,6 +138,8 @@ export function getVueVersionFromPackageJson(): 'vue2' | 'vue3' | undefined {
       if (majorVersion === 2) return 'vue2'
       if (majorVersion === 3) return 'vue3'
     }
+    // 处理关键字版本
+    if (vueVersion.includes('latest')) return 'vue3'
     // 如果版本号不匹配 vue2 或 vue3
     window.showWarningMessage(isEn ? 'the Vue version used by the project is not Vue 2 or Vue 3' : '项目使用的 Vue 版本不是 Vue 2 或 Vue 3')
   } catch (error: any) {
